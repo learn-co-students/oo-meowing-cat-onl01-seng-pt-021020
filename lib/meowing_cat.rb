29ca31a35b604c1bf00744800406850010c7cd4b
Terry Threatt <terry.threatt@gmail.com>
2020-02-25 17:01:56 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
 ## code your solution here. 
+class Cat 
+  attr_accessor :name 
+  
+  def meow
+    puts "meow!"
+  end 
+end 
+
+class Dog 
+  attr_accessor :name 
+  
+  def bark 
+    puts "woof!"
+  end 
+end 
